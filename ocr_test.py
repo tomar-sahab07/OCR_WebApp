409a33d0a3629b156aff5694c864bf7ec8ca871b
Aditya Tomar <104296617+tomar-sahab07@users.noreply.github.com>
2024-09-25 01:37:58 +0530

    Update ocr_test.py

--- ocr_test.py
+++ ocr_test.py
@@ -1,16 +1,7 @@
-# testing the image to text model here...
-
-from PIL import Image
 import pytesseract
+from PIL import Image
 
-# Setting the path to the Tesseract executable
-pytesseract.pytesseract.tesseract_cmd = r"C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
-
-# Loading an image from local machine
-image = Image.open("C:\\Users\\tomar\\OneDrive\\Documents\\hin_img.jpg")
-
-# Using pytesseract to extract text from the image
-extracted_text = pytesseract.image_to_string(image, lang='eng+hin')
-
-# Printing the extracted text
-print("Extracted Text:\n", extracted_text)
+def extract_text(image_file):
+    img = Image.open(image_file)
+    text = pytesseract.image_to_string(img, lang='eng+hin')  # OCR for both English and Hindi
+    return text
